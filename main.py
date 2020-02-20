48d0aee4495a2e1e781e6aef42aae85d3856a0e8
Kiara <tycho01@pm.me>
2020-02-20 19:09:31 +0100

    read in list of lists of ints

--- main.py
+++ main.py
@@ -8,11 +8,21 @@ class Foo:
 
 def main():
     fpath = './data/a_example.txt'
+    data_num_lists = read_lib(fpath)
+    print_lib(data_num_lists)
+
+def read_lib(fpath):
     with open(fpath) as f:
         data_str = f.read()
-        data_lines = data_str.splitlines()
-        for line in data_lines:
-            print(line)
+    data_lines = data_str.splitlines()
+    data_num_lists = [list(map(int, line.split(' '))) for line in data_lines]
+    return data_num_lists
+
+def print_lib(lib):
+    for line in lib:
+        for i in line:
+            print(i, end=' ')
+        print('')
 
 if __name__ == '__main__':
     main()
